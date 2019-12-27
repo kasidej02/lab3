9fc3ebdf8fe02674b0753596e953dc482807fb56
lab314-13@labeng.local <lab314-13@labeng.local>
2019-12-27 15:56:08 +0700

    Maybe finish

--- lab3_3.cpp
+++ lab3_3.cpp
@@ -3,10 +3,9 @@ using namespace std;
 int main(){
     int x=2,a;
     while(x<69){
-        a=(1/x)
-        x=x+1
-
+        a=(1/x)+a;
+        x=x+1;
     }
-    cout>>"Answer is"<<a;
+    cout<<"Answer is "<<a;
     return 0;
 }
