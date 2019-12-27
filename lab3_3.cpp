4acf8a3a9cb360e9468121356f964b7e7309e224
lab314-13@labeng.local <lab314-13@labeng.local>
2019-12-27 15:45:29 +0700

    3 edit

--- lab3_3.cpp
+++ lab3_3.cpp
@@ -1,12 +1,12 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int x;
-    cout>>"yeye";
-    
+    int x=2,a;
+    while(x<69){
+        a=(1/x)
+        x=x+1
 
-        
-    
-  
+    }
+    cout>>"Answer is"<<a;
     return 0;
 }
